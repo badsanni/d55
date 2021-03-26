85f1a7b003c6ac13f248ff79aefd9cc434dc9149
badsanni <femidba14@gmail.com>
2021-03-25 23:38:23 -0400

    getting used to it

--- script/frontend.sh
+++ script/frontend.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
 
-UID=$(id -u)
-if [ "${UID}"  -ne 0]; then
-    echo "You should be root user to perform this command"
-    exit 1
-fi
+echo "--------------------------------------------------------------------------------------------------------"
+echo -e "\e[1;35m [INFO] Installing Nginx\e[0m"
+echo "--------------------------------------------------------------------------------------------------------"
 
 yum install nginx -y
-systemctl enable nginx 
-systemctl start nginx 
+if [ $?  -ne 0]; then
+    echo "--------------------------------------------------------------------------------------------------------"
+    echo -e "\e[1;31m [ERROR] Nginx Installation has failed\e[0m"
+    echo "--------------------------------------------------------------------------------------------------------"
+    exit 2
+else 
+    echo "--------------------------------------------------------------------------------------------------------"
+    echo -e "\e[1;32m [SUCCESS] Nginx Installation Successful\e[0m"
+fi
+echo "--------------------------------------------------------------------------------------------------------"
+
+echo "--------------------------------------------------------------------------------------------------------"
+echo -e "\e[1;35m [INFO] Downloading frontend file into tmp directory\e[0m"
+echo "--------------------------------------------------------------------------------------------------------"
+
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+if [ $?  -ne 0]; then
+    echo "--------------------------------------------------------------------------------------------------------"
+    echo -e "\e[1;31m [ERROR] Download has failed\e[0m"
+    echo "--------------------------------------------------------------------------------------------------------"
+    exit 2
+else 
+    echo "--------------------------------------------------------------------------------------------------------"
+    echo -e "\e[1;32m [SUCCESS] Download is successfull Successful\e[0m"
+fi
+
+echo "--------------------------------------------------------------------------------------------------------"
 
 cd /usr/share/nginx/html 
 
@@ -20,5 +42,8 @@ mv frontend-main/* .
 mv static/* .
 rm -rf frontend-master README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+systemctl enable nginx 
+systemctl start nginx 
 
 systemctl restart nginx
